dd77376d51afea16e7ff31a17cb988ce434a76bc
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-02 10:18:35 +0100

    fix bug in template

--- Problem1/tests.py
+++ Problem1/tests.py
@@ -2,6 +2,15 @@ import unittest
 from solution import solver
 
 class getResults(unittest.TestCase):
+    def test_onlyTwoNumbers(self):
+        testdata  = [2019,1]
+        cut = solver( testdata )
+        self.assertEquals(cut.getResultForTwo(), 2019)
+    
+    def test_ThreeNumbers(self):
+        testdata  = [1,20, 2000]
+        cut = solver( testdata )
+        self.assertEquals(cut.getResultForTwo(), 2000*20)
 
 if __name__ == '__main__':
     unittest.main()
