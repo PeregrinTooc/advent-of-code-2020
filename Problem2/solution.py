f8139d2b67eedd7757ffbb858f756af16bfe268c
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-03 08:08:51 +0100

    Solution Day 2

--- Problem2/solution.py
+++ Problem2/solution.py
@@ -1,5 +1,36 @@
-class solver:
-    pass
+class abstractSolver:
+    def setPasswords(self, passwords):
+        self.passwords = passwords
+    
+    def solve(self):
+        result = 0
+        for password in self.passwords:
+            if self.isValidPassword(password):
+                result += 1 
+        return result
+class solver1(abstractSolver):
+    def isValidPassword(self, passwordWithParameters):
+        lower = passwordWithParameters[0]
+        upper = passwordWithParameters[1]
+        char = passwordWithParameters[2]
+        password = passwordWithParameters[3]
+
+        if password.count(char) < lower or password.count(char) > upper:
+            return False
+        else:
+            return True
+
+class solver2(abstractSolver):
+    def isValidPassword(self, passwordWithParameters):
+        lower = passwordWithParameters[0]
+        upper = passwordWithParameters[1]
+        char = passwordWithParameters[2]
+        password = passwordWithParameters[3]
+
+        if ( password[lower-1]==char ) ^ ( password[upper-1]==char ):
+            return True
+        else:
+            return False
 
 def parseFile():
     import os
@@ -12,9 +43,25 @@ def parseFile():
     return result
 
 def parsePuzzleInputFrom(file):
-    pass
+    result = []
+    input = file.readlines()
+    for i in range(len(input)):
+        result.append( parseLine(input[i].rstrip('\n')))
+    return result
+
+def parseLine(line):
+    result = []
+    result.append(int(line[0:line.find('-')]))
+    line = line[line.find('-')+1:]
+    result.append(int(line[0:line.find(' ')]))
+    line = line[line.find(' ')+1:]
+    result.append(line[0])
+    result.append(line[3:])
+    return result
 
 if __name__ == '__main__':
     puzzleInput = parseFile( )
-    print(solver(puzzleInput).getResultForThree())
+    solver = solver2()
+    solver.setPasswords(puzzleInput)
+    print(solver.solve())
 
--- Problem2/tests.py
+++ Problem2/tests.py
@@ -1,21 +1,83 @@
 import unittest
-from solution  import solver
+from solution  import solver1
+from solution  import solver2
 from solution import parsePuzzleInputFrom
 
+class isValidPassword(unittest.TestCase):
+    def setUp(self):
+        self.cut = solver2()
+
+    def test_empty_input_invalid(self):
+        with self.assertRaises( IndexError ):
+            self.cut.isValidPassword( [] ) 
+    
+    def test_invalid_due_to_not_there(self):
+        self.assertFalse( self.cut.isValidPassword( [1,2,'a','bc'] ) )
+    
+    def test_valid_pos1(self):
+        self.assertTrue( self.cut.isValidPassword( [1,2,'a','ab'] ) )
+
+    def test_valid_pos2(self):
+        self.assertTrue( self.cut.isValidPassword( [1,2,'a','ba'] ) )
+
+    def test_invalid_both_positions(self):
+        self.assertFalse( self.cut.isValidPassword( [1,2,'a','aa'] ) )
+
+
 class getResults(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.cut = solver1()
+
+    def test_empty_input_invalid(self):
+        with self.assertRaises( IndexError ):
+            self.cut.isValidPassword( [] ) 
+
+    def test_invalid_due_to_too_few(self):
+        self.assertFalse( self.cut.isValidPassword( [1,1,'a',''] ) )
+
+    def test_invalid_due_to_too_many(self):
+        self.assertFalse( self.cut.isValidPassword( [1,1,'a','aa'] ) )
+
+    def test_valid_minimal(self):
+        self.assertTrue( self.cut.isValidPassword( [1,1,'a','a'] ) )
+    
+    def test_solve_to_0(self):
+        self.cut.setPasswords( [[1,1,'a',''],[1,1,'a','aa']] )
+        self.assertEqual(self.cut.solve(),0)
+
+    def test_solve_to_1(self):
+        self.cut.setPasswords( [[1,1,'a',''],[1,1,'a','a']] )
+        self.assertEqual(self.cut.solve(),1)
+
 
 class parseFile(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         import os
         relPath = 'PuzzleInputTest.txt'
         absFilePath = os.path.dirname(__file__)
         absFilePath = os.path.join(absFilePath, relPath)
         self.file = open(absFilePath, 'r')
-
-    def tearDown(self) -> None:
+        self.result = parsePuzzleInputFrom(self.file)
+    def tearDown(self):
         self.file.close()
+    
+    def test_parse(self):
+        self.assertEqual( self.result, [[1,3,'a','abcde'],[11,12,'x','xxxxxxxxxxxz']])
+    
+    def test_length(self):
+        self.assertEqual( len(self.result),2 )
 
+    def test_firstElement(self):
+        self.assertEqual( self.result[0][0],1)
+        self.assertEqual( self.result[1][0],11)
+    
+    def test_secondElement(self):
+        self.assertEqual( self.result[0][1],3)
+        self.assertEqual( self.result[1][1],12)
+    
+    def test_thirdElement(self):
+        self.assertEqual( self.result[0][2],'a')
+        self.assertEqual( self.result[1][2],'x')
     
 if __name__ == '__main__':
     unittest.main()
