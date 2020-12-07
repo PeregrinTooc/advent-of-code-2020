8a382f48e2c9e63899b9adf35c2454e81a1634d9
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-07 13:25:44 +0100

    Solution 4

--- Problem4/solution.py
+++ Problem4/solution.py
@@ -1,5 +1,82 @@
+class passport:
+    def __init__(self, attributes ):
+        self.invalid = False
+        try:
+            self.countryID = attributes['cid']
+        except KeyError:
+            self.invalid = False
+        try:
+            self.passid = attributes['pid']
+            self.issueYear = attributes['iyr']
+            self.expirationYear = attributes['eyr']
+            self.birthYear = attributes['byr']
+            self.height = attributes['hgt']
+            self.eyecolor = attributes['ecl']
+            self.hairColor = attributes['hcl']
+        except KeyError: 
+            self.invalid = True
+        if not self.invalid:
+            self.validate()
+    
+    def validate(self):
+        self.validateYears()
+        self.validateHeight()
+        self.validateHairColor()
+        self.validateEyeColor()
+        self.validatePassportID()
+
+    def validateHeight(self):
+        try:
+            if len(self.height) == 4:
+                if self.height[2:] != 'in':
+                    self.invalid = True
+                elif int(self.height[0:2]) < 59 or int(self.height[0:2]) > 76:
+                    self.invalid = True
+            elif len(self.height) == 5:
+                if self.height[3:] != 'cm':
+                    self.invalid = True
+                elif int(self.height[0:3]) < 150 or int(self.height[0:3]) > 193:
+                    self.invalid = True
+            else:
+                self.invalid = True
+        except ValueError:
+            self.invalid = True
+
+    def validateHairColor(self):
+        import string
+        if self.hairColor[0] != '#' or len(self.hairColor) != 7:
+            self.invalid = True 
+        elif not ( set(self.hairColor[1:]) <= set(string.digits+'abcdef') ):
+            self.invalid = True
+
+    def validateEyeColor(self):
+        if not self.eyecolor in set(['amb','blu','brn','gry','grn','hzl','oth']):
+            self.invalid = True
+
+    def validatePassportID(self):
+        if len(self.passid) != 9:
+            self.invalid = True
+        try:
+            int(self.passid)
+        except ValueError:
+            self.invalid = True
+
+    def validateYears(self):
+        if int(self.birthYear)>2002 or int(self.birthYear)<1920:
+            self.invalid = True
+        if int(self.issueYear)>2020 or int(self.issueYear)<2010:
+            self.invalid = True
+        if int(self.expirationYear)>2030 or int(self.expirationYear)<2020:
+            self.invalid = True
 class solver:
-    pass
+    def __init__(self,puzzleInput):
+        self.passports = puzzleInput
+    def solve(self):    
+        result = 0
+        for passport in self.passports:
+            if not passport.invalid:
+                result += 1
+        return result
 
 def parseFile():
     import os
@@ -13,7 +90,31 @@ def parseFile():
     return result
 
 def parsePuzzleInputFrom(file):
-    pass
+    result = []
+    passports = file.readlines()
+    attributes = {}
+    for i in range(len(passports)):
+        line = passports[i].strip('\n')
+        if len(line) == 0:
+            result.append(passport(attributes))
+            del attributes
+            attributes = {}
+        else:    
+            while len(line)>0:
+                space = line.find(' ')
+                delimiter = line.find(':')
+                if space == -1:
+                    kwAndValue = line
+                    line = ''
+                else:    
+                    kwAndValue = line[0:space]
+                    line = line[space+1:]
+                kw = kwAndValue[0:delimiter]
+                value = kwAndValue[delimiter+1:] 
+                attributes[kw] = value
+    result.append(passport(attributes))
+    return result
+
 
 if __name__ == '__main__':
     puzzleInput = parseFile( )
--- Problem4/tests.py
+++ Problem4/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from solution  import solver
 from solution import parsePuzzleInputFrom
+from solution import passport
 
 class getResults(unittest.TestCase):
     pass
@@ -12,10 +13,114 @@ class parseFile(unittest.TestCase):
         absFilePath = os.path.dirname(__file__)
         absFilePath = os.path.join(absFilePath, relPath)
         self.file = open(absFilePath, 'r')
-
-    def tearDown(self) -> None:
+        self.result = parsePuzzleInputFrom(self.file)
         self.file.close()
 
+    def tearDown(self) -> None:
+        pass
+
+    def test_return_type(self):
+        self.assertIsInstance(self.result[0], passport)
+
+    def test_return_count(self):
+        self.assertEqual(len(self.result),4)
+
+    def test_passport_parsing(self):
+        passport = self.result[0]
+        self.assertEqual(passport.eyecolor, 'gry')
+        self.assertEqual(passport.passid, '860033327')
+        self.assertEqual(passport.expirationYear, '2020')
+        self.assertEqual(passport.hairColor, '#fffffd')
+        self.assertEqual(passport.birthYear, '1937')
+        self.assertEqual(passport.issueYear, '2017')
+        self.assertEqual(passport.countryID, '147')
+        self.assertEqual(passport.height, '183cm')
+        passport = self.result[1]
+        self.assertEqual(passport.invalid, True)
+        passport = self.result[2]
+        self.assertEqual(passport.eyecolor, 'brn')
+        self.assertEqual(passport.passid, '760753108')
+        self.assertEqual(passport.expirationYear, '2024')
+        self.assertEqual(passport.hairColor, '#ae17e1')
+        self.assertEqual(passport.birthYear, '1931')
+        self.assertEqual(passport.issueYear, '2013')
+        self.assertEqual(passport.height, '179cm')
+        self.assertEqual(passport.invalid, False)
+        passport = self.result[3]
+        self.assertEqual(passport.invalid, True)
+
+class validations(unittest.TestCase):
+    def setUp(self):
+        self.attributes = {'ecl':'gry','pid':'860033327','eyr':'2020','hcl':'#fffffd','byr':'1920','iyr':'2010','cid':'147','hgt':'183cm'}
+        self.assertValid()
+    def assertValid(self):
+        cut = passport(self.attributes)
+        self.assertFalse(cut.invalid)
+    def assertInvalid(self):
+        cut = passport(self.attributes)
+        self.assertTrue(cut.invalid)
     
+    def test_BirthYearTooLow(self):
+        self.attributes['byr'] = '1919'
+        self.assertInvalid()
+    def test_BirthYearTooHigh(self):
+        self.attributes['byr'] = '2003'
+        self.assertInvalid()
+    def test_BirthYearFitsUpper(self):
+        self.attributes['byr'] = '2002'
+        self.assertValid()
+    
+    def test_IssueYearTooLow(self):
+        self.attributes['iyr'] = '2009'
+        self.assertInvalid()
+    def test_IssueYearTooHigh(self):
+        self.attributes['iyr'] = '2021'
+        self.assertInvalid()
+    def test_IssueYearFitsUpper(self):
+        self.attributes['iyr'] = '2020'
+        self.assertValid()
+
+    def test_ExpirationYearTooLow(self):
+        self.attributes['eyr'] = '2019'
+        self.assertInvalid()
+    def test_ExpirationYearTooHigh(self):
+        self.attributes['eyr'] = '2031'
+        self.assertInvalid()
+    def test_ExpirationYearFitsUpper(self):
+        self.attributes['eyr'] = '2030'
+        self.assertValid()
+
+    def test_HeightNeitherInchNorCm(self):
+        self.attributes['hgt'] = '123dm'
+        self.assertInvalid()
+        self.attributes['hgt'] = '23dm'
+        self.assertInvalid()
+
+    def test_TooBig(self):
+        self.attributes['hgt'] = '194cm'
+        self.assertInvalid()
+        self.attributes['hgt'] = '77in'
+        self.assertInvalid()
+
+    def test_TooSmall(self):
+        self.attributes['hgt'] = '149cm'
+        self.assertInvalid()
+        self.attributes['hgt'] = '58in'
+        self.assertInvalid()
+
+    def test_JustNotTooBigNorTooSmall(self):
+        self.attributes['hgt'] = '150cm'
+        self.assertValid()
+        self.attributes['hgt'] = '59in'
+        self.assertValid()
+        self.attributes['hgt'] = '193cm'
+        self.assertValid()
+        self.attributes['hgt'] = '76in'
+        self.assertValid()
+
+    def test_invalidHairColor(self):
+        self.attributes['hcl'] = '123456d'
+        self.assertInvalid()
+
 if __name__ == '__main__':
     unittest.main()
