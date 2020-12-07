6bb14fb04c36c858ca0ae6224e374117240d6222
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-07 15:12:26 +0100

    Solve 5

--- Problem5/solution.py
+++ Problem5/solution.py
@@ -1,5 +1,51 @@
+import math
+class seatParser:
+    def __init__(self, code):
+        self.row = code[:7]
+        self.seat = code[7:]
+
+    def getRow(self):
+        result = 0
+        for i in range(len(self.row)):
+            if self.row[i] == 'F':
+               result += 0   
+            else:
+                result += 128/math.pow(2,i+1)
+        return result
+
+    def getSeat(self):
+        result = 0
+        for i in range(len(self.seat)):
+            if self.seat[i] == 'L':
+               result += 0   
+            else:
+                result += 8/math.pow(2,i+1)
+        return result
+    
+    def getSeatId(self):
+        return self.getRow()*8+self.getSeat()
 class solver:
-    pass
+    def __init__(self,seats):
+        self.seats = seats
+    
+    def solve(self):
+        result = 0
+        for seat in self.seats:
+            currentId = seatParser(seat).getSeatId()
+            if currentId > result:
+                result = currentId
+        return result
+    
+    def findGap(self):
+        result = 0
+        seatIDs = []
+        for seat in self.seats:
+            seatIDs.append(seatParser(seat).getSeatId())
+        seatIDs.sort()
+        for i in (range(len(seatIDs))):
+            if seatIDs[i+1] - seatIDs[i] == 2:
+              return seatIDs[i]+1  
+    
 
 def parseFile():
     import os
@@ -13,9 +59,13 @@ def parseFile():
     return result
 
 def parsePuzzleInputFrom(file):
-    pass
+    result = []
+    for line in file.readlines():
+        result.append(line.strip('\n'))
+    return result
 
 if __name__ == '__main__':
     puzzleInput = parseFile( )
     print(solver(puzzleInput).solve())
+    print(solver(puzzleInput).findGap())
 
--- Problem5/tests.py
+++ Problem5/tests.py
@@ -1,9 +1,53 @@
 import unittest
 from solution  import solver
 from solution import parsePuzzleInputFrom
+from solution import seatParser
 
-class getResults(unittest.TestCase):
-    pass
+class getRow(unittest.TestCase):
+    def test_FBFBBFFRLR(self):
+        actualRow = seatParser('FBFBBFFRLR').getRow()
+        expectedRow = 44
+        self.assertEqual(actualRow, expectedRow)
+    
+    def test_FFFFFFFLLL(self):
+        actualRow = seatParser('FFFFFFFLLL').getRow()
+        expectedRow = 0
+        self.assertEqual(actualRow, expectedRow)
+
+    def test_BFFFFFFLLL(self):
+        actualRow = seatParser('BFFFFFFLLL').getRow()
+        expectedRow = 64
+        self.assertEqual(actualRow, expectedRow)
+
+    def test_BBFFFFFLLL(self):
+        actualRow = seatParser('BBFFFFFLLL').getRow()
+        expectedRow = 96
+        self.assertEqual(actualRow, expectedRow)
+
+class getSeatNumber(unittest.TestCase):
+    def test_FFFFFFFLLL(self):
+        actualSeat = seatParser('FFFFFFFLLL').getSeat()
+        expectedSeat = 0
+        self.assertEqual(actualSeat, expectedSeat)
+
+    def test_FFFFFFFRLL(self):
+        actualSeat = seatParser('FFFFFFFRLL').getSeat()
+        expectedSeat = 4
+        self.assertEqual(actualSeat, expectedSeat)
+
+    def test_FFFFFFFLRL(self):
+        actualSeat = seatParser('FFFFFFFLRL').getSeat()
+        expectedSeat = 2
+        self.assertEqual(actualSeat, expectedSeat)
+
+class getSeatId(unittest.TestCase):
+    def test_MultipleIds(self):
+        self.assertEqual(seatParser('FBFBBFFRLR').getSeatId(),357)
+        self.assertEqual(seatParser('BFFFBBFRRR').getSeatId(),567)
+        self.assertEqual(seatParser('FFFBBBFRRR').getSeatId(),119)
+        self.assertEqual(seatParser('BBFFBBFRLL').getSeatId(),820)
+        self.assertEqual(seatParser('FFFFFFFLLL').getSeatId(),0)
+        self.assertEqual(seatParser('BBBBBBBRRR').getSeatId(),1023)
 
 class parseFile(unittest.TestCase):
     def setUp(self) -> None:
@@ -11,11 +55,15 @@ class parseFile(unittest.TestCase):
         relPath = 'PuzzleInputTest.txt'
         absFilePath = os.path.dirname(__file__)
         absFilePath = os.path.join(absFilePath, relPath)
-        self.file = open(absFilePath, 'r')
+        file = open(absFilePath, 'r')
+        self.parsed = parsePuzzleInputFrom(file)
+        file.close()
 
-    def tearDown(self) -> None:
-        self.file.close()
-
-    
+    def test_firstLine(self):
+        self.assertEqual('FBFBBFFRLR',self.parsed[0])
+    def test_secondLine(self):
+        self.assertEqual('BFFFBBFRRR',self.parsed[1])
+    def test_solver(self):
+        self.assertEqual(solver(self.parsed).solve(),820)
 if __name__ == '__main__':
     unittest.main()
