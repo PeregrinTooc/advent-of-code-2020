c5b1a3a361c79bdee9be5f3a4a2a192fae6b5acc
philipp <philipp.rembold@gmail.com>
2020-12-14 23:05:24 +0100

    Solution8

--- Problem8/solution.py
+++ Problem8/solution.py
@@ -1,5 +1,57 @@
-class solver:
+class repeatInstructionException(Exception):
     pass
+
+class codeFixer:
+    def __init__(self,code):
+        self.code = code
+        self.switchedLine = 0 
+    
+    def switch(self):
+        line = self.switchedLine   
+        self.switchJmpAndNop(line)
+        if line > 0:
+            line -= 1
+            self.switchJmpAndNop(line)
+
+        self.switchedLine += 1 
+
+    def switchJmpAndNop(self, line):
+        if self.code[line][0:3] == 'jmp':
+            self.code[line] = self.code[line].replace('jmp', 'nop')
+        elif self.code[line][0:3] == 'nop':
+            self.code[line] = self.code[line].replace('nop', 'jmp')
+
+    def getCode(self):
+        return  self.code
+class solver:
+    def __init__(self,code):
+        self.acc = 0
+        self.line = 0
+        self.code = code
+    def getAcc(self):
+        return self.acc
+    def tick(self):
+        command = self.code[self.line]
+        if command.find('proc') > -1:
+            raise repeatInstructionException()
+        self.code[self.line] += 'proc'
+
+        if command[:3] == 'nop':
+          self.line +=1
+          return
+        if command[:3] == 'jmp':
+            if command.find('+') == -1:            
+                self.line += int(command[command.find('-'):])
+            else:
+                self.line += int(command[command.find('+'):])
+            return
+        if command.find('+') == -1:
+            self.acc += int(command[command.find('-'):])
+        else:
+            self.acc += int(command[command.find('+'):])
+        self.line += 1
+        return
+        
 
 def parseFile():
     import os
@@ -13,9 +65,27 @@ def parseFile():
     return result
 
 def parsePuzzleInputFrom(file):
-    pass
+    result = []
+    lines = file.readlines()
+    for line in lines:
+        result.append(line.strip('\n'))
+    return result
 
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
-    print(solver(puzzleInput).solve())
+    initialCode = parseFile( )
+    for i in range(len(initialCode)):        
+        code = initialCode.copy()
+        codeDebgugger = codeFixer(code)        
+        codeDebgugger.switchJmpAndNop(i)
+        code = codeDebgugger.getCode()
+        program = solver(code)        
+        try:
+            while True:
+                program.tick()
+        except repeatInstructionException:
+            continue
+        except IndexError:
+            break
+
+    print(program.getAcc())
 
--- Problem8/tests.py
+++ Problem8/tests.py
@@ -1,6 +1,8 @@
 import unittest
-from solution  import solver
+from solution import solver
 from solution import parsePuzzleInputFrom
+from solution import repeatInstructionException
+from solution import codeFixer
 
 def parseFile(filename = 'PuzzleInputTest.txt'):
     import os
@@ -11,13 +13,93 @@ def parseFile(filename = 'PuzzleInputTest.txt'):
         result = parsePuzzleInputFrom(file)
     return result
 
+class findCorruptInstruction(unittest.TestCase):
+    def setUp(self):
+        self.parsedInput = parseFile('PuzzleInputTest2.txt')    
+        self.cut = codeFixer(self.parsedInput)
+
+    def test_switchFirstInstruction(self):
+        self.switch(1)
+        self.assertEqual(self.cut.getCode()[0],'jmp +0') 
+
+    def test_switchSecondInstruction(self):       
+        self.switch(3)
+        self.assertEqual(self.cut.getCode()[2],'nop +4') 
+    
+    def test_switchSecondInstructionFirstIsSwitchedBack(self):        
+        self.switch(3)
+        self.assertEqual(self.cut.getCode()[0],'nop +0') 
+    
+    def test_switchFourTimesNoChange(self):
+        self.switch(4)
+        self.assertEqual(self.cut.getCode(),self.parsedInput) 
+
+    def test_changedCodeStillRaisesException(self):
+        with self.assertRaises(repeatInstructionException):
+            self.switch(3)
+            slv = solver(self.cut.getCode())
+            for _ in range(10):
+                slv.tick()
+
+    def test_correctChangeNoException(self):
+        self.switch(8)
+        slv = solver(self.cut.getCode())
+        with self.assertRaises(IndexError):
+            while True:
+                slv.tick()
+        self.assertEqual(slv.getAcc(),8)   
+    
+    def switch(self, times):
+        for _ in range(times):
+            self.cut.switch()
+  
+
 class getResults(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.parsedInput = parseFile()
+        self.cut = solver(self.parsedInput)
+
+    def test_initialValue(self):
+        self.assertEqual(self.cut.getAcc(),0)
+    
+    def test_nopStep(self):
+        self.tickTimes(1)
+        self.assertEqual(self.cut.getAcc(),0)
+
+    def tickTimes(self,n):    
+        for _ in range(n):
+            self.cut.tick()
+
+    def test_accRaisedForAcc(self):
+        self.tickTimes(2)
+        self.assertEqual(self.cut.getAcc(),1)
+
+    def test_jump(self):
+        self.tickTimes(4)
+        self.assertEqual(self.cut.getAcc(),2)
+    
+    def test_negativeJump(self):
+        self.tickTimes(6)
+        self.assertEqual(self.cut.getAcc(),-1)
+    
+    def test_tickAbortsOnRepetition(self):
+        with self.assertRaises(repeatInstructionException):
+            self.tickTimes(10)
+        self.assertEqual(self.cut.getAcc(),-1)
+
+
+    
+
 
 class parsePuzzleInput(unittest.TestCase):
     def setUp(self) -> None:
         self.parsedInput = parseFile()
 
+    def test_line1(self):
+        self.assertEqual(self.parsedInput[0],"nop +0")
     
+    def test_line2(self):
+        self.assertEqual(self.parsedInput[1],"acc +1")
+
 if __name__ == '__main__':
     unittest.main()
