fce017835e21e7d5c11f2666ccc2b8b6ffe0fbee
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-15 10:15:27 +0100

    Refactor 7 and 5

--- Problem5/solution.py
+++ Problem5/solution.py
@@ -1,4 +1,6 @@
 import math
+
+
 class seatParser:
     def __init__(self, code):
         self.row = code[:7]
@@ -8,26 +10,28 @@ class seatParser:
         result = 0
         for i in range(len(self.row)):
             if self.row[i] == 'F':
-               result += 0   
+                result += 0
             else:
-                result += 128/math.pow(2,i+1)
+                result += 128/math.pow(2, i+1)
         return result
 
     def getSeat(self):
         result = 0
         for i in range(len(self.seat)):
             if self.seat[i] == 'L':
-               result += 0   
+                result += 0
             else:
-                result += 8/math.pow(2,i+1)
+                result += 8/math.pow(2, i+1)
         return result
-    
+
     def getSeatId(self):
         return self.getRow()*8+self.getSeat()
+
+
 class solver:
-    def __init__(self,seats):
+    def __init__(self, seats):
         self.seats = seats
-    
+
     def solve(self):
         result = 0
         for seat in self.seats:
@@ -35,7 +39,7 @@ class solver:
             if currentId > result:
                 result = currentId
         return result
-    
+
     def findGap(self):
         result = 0
         seatIDs = []
@@ -44,19 +48,19 @@ class solver:
         seatIDs.sort()
         for i in (range(len(seatIDs))):
             if seatIDs[i+1] - seatIDs[i] == 2:
-              return seatIDs[i]+1  
-    
+                return seatIDs[i]+1
+
 
 def parseFile():
     import os
     relPath = 'PuzzleInput.txt'
-    absFilePath = os.path.abspath(__file__)
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
+
 
 def parsePuzzleInputFrom(file):
     result = []
@@ -64,8 +68,8 @@ def parsePuzzleInputFrom(file):
         result.append(line.strip('\n'))
     return result
 
+
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
+    puzzleInput = parseFile()
     print(solver(puzzleInput).solve())
     print(solver(puzzleInput).findGap())
-
--- Problem7/solution.py
+++ Problem7/solution.py
@@ -1,49 +1,53 @@
 class solver:
     def __init__(self, rules):
-      self.rules = rules
-      self.bagColors = []
+        self.rules = rules
+        self.bagColors = []
 
-    def solve(self,bagColor)->int:
+    def solve(self, bagColor) -> int:
         self.resolveHierarchy(bagColor)
         return len(set(self.bagColors))
-   
-    def resolveHierarchy(self,bagColor):
+
+    def resolveHierarchy(self, bagColor):
         for rule in self.rules:
             for contained in rule.getContained():
                 if contained['bag'].getColor() == bagColor:
                     self.bagColors.append(rule.getContainer().getColor())
                     self.resolveHierarchy(rule.getContainer().getColor())
 
-    def countContainedFor(self,bagColor):
+    def countContainedFor(self, bagColor):
         result = 0
         currentRule = self.getRuleFor(bagColor)
         for contained in currentRule.getContained():
             result += contained['count']
-            result += ( contained['count']*self.countContainedFor(contained['bag'].getColor()) )
-        return result   
+            result += (contained['count'] *
+                       self.countContainedFor(contained['bag'].getColor()))
+        return result
 
     def getRuleFor(self, bagColor):
         for rule in self.rules:
             if bagColor == rule.getContainer().getColor():
                 return rule
 
+
 class bag:
-    def __init__(self,color):
+    def __init__(self, color):
         self.color = color
-    def getColor(self)->str:
+
+    def getColor(self) -> str:
         return self.color
+
 
 class rule:
     def __init__(self) -> None:
         self.contained = []
 
-    def getContainer(self)->bag:
+    def getContainer(self) -> bag:
         return self.container
-    
-    def setContainer(self,color):
+
+    def setContainer(self, color):
         self.container = bag(color)
 
-    def addContained(self,contained):
+    def addContained(self, contained):
         self.contained.append(contained)
 
     def getContained(self) -> list:
@@ -53,41 +57,46 @@ class rule:
 def parseFile():
     import os
     relPath = 'PuzzleInput.txt'
-    absFilePath = os.path.abspath(__file__)
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
+
 
 def parsePuzzleInputFrom(file):
     result = []
     for line in file.readlines():
         noMoreBags, line, currentRule = initialize(line)
-        delimiter = createRuleAndGetDelimiter(line, currentRule) 
-        noMoreBags = thereAreNoBagsContained(line, noMoreBags)  
+        delimiter = createRuleAndGetDelimiter(line, currentRule)
+        noMoreBags = thereAreNoBagsContained(line, noMoreBags)
         line = line[delimiter+len(' bags contain '):]
         while(not noMoreBags):
             line, count = getCount(line)
-            delimiter, noMoreBags = getDelimiterForContainedBags(line, delimiter)
+            delimiter, noMoreBags = getDelimiterForContainedBags(
+                line, delimiter)
             bagColor, line = getBagColor(line, delimiter)
-            currentRule.addContained({'count' : int(count), 'bag' : bag(bagColor)})        
+            currentRule.addContained(
+                {'count': int(count), 'bag': bag(bagColor)})
         result.append(currentRule)
     return result
 
+
 def getBagColor(line, delimiter):
-    bagDescription= line[:delimiter]
+    bagDescription = line[:delimiter]
     line = line[delimiter+len(', '):]
     bagDescription, bagColor = getCount(bagDescription)
     delimiter = bagDescription.find(' ')
-    bagColor =bagColor+' '+bagDescription[:delimiter]
+    bagColor = bagColor+' '+bagDescription[:delimiter]
     return bagColor, line
+
 
 def thereAreNoBagsContained(line, result):
     if line.find('no other bags') > -1:
-      result = True  
+        result = True
     return result
+
 
 def getDelimiterForContainedBags(line, delimiter):
     exit = False
@@ -97,17 +106,20 @@ def getDelimiterForContainedBags(line, delimiter):
         exit = True
     return delimiter, exit
 
+
 def getCount(line):
     delimiter = line.find(' ')
     count = line[:delimiter]
     line = line[delimiter+1:]
     return line, count
 
+
 def createRuleAndGetDelimiter(line, currentRule):
     delimiter = line.find(' bags contain ')
     bagColor = line[:delimiter]
-    currentRule.setContainer(bagColor) 
+    currentRule.setContainer(bagColor)
     return delimiter
+
 
 def initialize(line):
     exit = False
@@ -115,8 +127,8 @@ def initialize(line):
     currentRule = rule()
     return exit, line, currentRule
 
+
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
+    puzzleInput = parseFile()
     print(solver(puzzleInput).solve("shiny gold"))
     print(solver(puzzleInput).countContainedFor("shiny gold"))
-
