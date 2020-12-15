a139697f55d91b2693964b6a9dfd582ae7773142
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-15 09:59:26 +0100

    Refactor 8

--- Problem8/solution.py
+++ Problem8/solution.py
@@ -1,19 +1,20 @@
 class repeatInstructionException(Exception):
     pass
 
+
 class codeFixer:
-    def __init__(self,code):
+    def __init__(self, code):
         self.code = code
-        self.switchedLine = 0 
-    
+        self.switchedLine = 0
+
     def switch(self):
-        line = self.switchedLine   
+        line = self.switchedLine
         self.switchJmpAndNop(line)
         if line > 0:
             line -= 1
             self.switchJmpAndNop(line)
 
-        self.switchedLine += 1 
+        self.switchedLine += 1
 
     def switchJmpAndNop(self, line):
         if self.code[line][0:3] == 'jmp':
@@ -22,36 +23,49 @@ class codeFixer:
             self.code[line] = self.code[line].replace('nop', 'jmp')
 
     def getCode(self):
-        return  self.code
+        return self.code
+
+
 class solver:
-    def __init__(self,code):
+    def __init__(self, code):
         self.acc = 0
         self.line = 0
         self.code = code
+
     def getAcc(self):
         return self.acc
+
     def tick(self):
         command = self.code[self.line]
+        self.handleAlreadyProcessed(command)
+        self.handleNopInstruction(command)
+        self.handleJmpInstruction(command)
+        self.handleAccInstruction(command)
+
+    def handleAccInstruction(self, command):
+        if command[:3] == 'acc':
+            if command.find('+') == -1:
+                self.acc += int(command[command.find('-'):])
+            else:
+                self.acc += int(command[command.find('+'):])
+            self.line += 1
+
+    def handleJmpInstruction(self, command):
+        if command[:3] == 'jmp':
+            if command.find('+') == -1:
+                self.line += int(command[command.find('-'):])
+            else:
+                self.line += int(command[command.find('+'):])
+
+    def handleNopInstruction(self, command):
+        if command[:3] == 'nop':
+            self.line += 1
+
+    def handleAlreadyProcessed(self, command):
         if command.find('proc') > -1:
             raise repeatInstructionException()
         self.code[self.line] += 'proc'
 
-        if command[:3] == 'nop':
-          self.line +=1
-          return
-        if command[:3] == 'jmp':
-            if command.find('+') == -1:            
-                self.line += int(command[command.find('-'):])
-            else:
-                self.line += int(command[command.find('+'):])
-            return
-        if command.find('+') == -1:
-            self.acc += int(command[command.find('-'):])
-        else:
-            self.acc += int(command[command.find('+'):])
-        self.line += 1
-        return
-        
 
 def parseFile():
     import os
@@ -60,9 +74,10 @@ def parseFile():
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
+
 
 def parsePuzzleInputFrom(file):
     result = []
@@ -71,14 +86,15 @@ def parsePuzzleInputFrom(file):
         result.append(line.strip('\n'))
     return result
 
+
 if __name__ == '__main__':
-    initialCode = parseFile( )
-    for i in range(len(initialCode)):        
+    initialCode = parseFile()
+    for i in range(len(initialCode)):
         code = initialCode.copy()
-        codeDebgugger = codeFixer(code)        
+        codeDebgugger = codeFixer(code)
         codeDebgugger.switchJmpAndNop(i)
         code = codeDebgugger.getCode()
-        program = solver(code)        
+        program = solver(code)
         try:
             while True:
                 program.tick()
@@ -88,4 +104,3 @@ if __name__ == '__main__':
             break
 
     print(program.getAcc())
-
