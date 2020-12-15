00947946329b959d2db3da6a0883da5e6a697316
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-15 12:29:53 +0100

    refactor 3

--- Problem3/solution.py
+++ Problem3/solution.py
@@ -1,12 +1,16 @@
 class solver:
-    def __init__(self,area):
+    def __init__(self, area):
+        self.initialize(area)
+        self.numberOfEncounteredTrees = 0
+        self.stepsRight = 3
+        self.stepsDown = 1
+
+    def initialize(self, area):
         self.area = area
         self.column = 0
         self.line = 0
         self.width = len(area[0])
-        self.numberOfEncounteredTrees = 0
-        self.stepsRight = 3
-        self.stepsDown = 1
+
     def step(self):
         self.column += self.stepsRight
         self.column %= self.width
@@ -15,7 +19,7 @@ class solver:
     def getPosition(self):
         return self.area[self.line][self.column]
 
-    def solve(self, stepsRight = 3, stepsDown = 1):
+    def solve(self, stepsRight=3, stepsDown=1):
         self.stepsRight = stepsRight
         self.stepsDown = stepsDown
         try:
@@ -26,6 +30,7 @@ class solver:
         except IndexError:
             return self.numberOfEncounteredTrees
 
+
 def parseFile():
     import os
     relPath = 'PuzzleInput.txt'
@@ -33,9 +38,10 @@ def parseFile():
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
+
 
 def parsePuzzleInputFrom(file):
     result = []
@@ -44,12 +50,11 @@ def parsePuzzleInputFrom(file):
         result.append(line.strip('\n'))
     return result
 
+
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
-    print(solver(puzzleInput).solve(1,1))
-    print(solver(puzzleInput).solve(3,1))
-    print(solver(puzzleInput).solve(5,1))
-    print(solver(puzzleInput).solve(7,1))
-    print(solver(puzzleInput).solve(1,2))
-
-
+    puzzleInput = parseFile()
+    print(solver(puzzleInput).solve(1, 1))
+    print(solver(puzzleInput).solve(3, 1))
+    print(solver(puzzleInput).solve(5, 1))
+    print(solver(puzzleInput).solve(7, 1))
+    print(solver(puzzleInput).solve(1, 2))
