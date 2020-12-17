23d7968eaefbd4f7c96b350104a25ce060ffb8ca
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-17 16:32:30 +0100

    solution 10 part 1

--- Problem10/solution.py
+++ Problem10/solution.py
@@ -1,21 +1,46 @@
+from types import GetSetDescriptorType
+
+
 class solver:
-    pass
+    def __init__(self, puzzleInput) -> None:
+        self.jolts = puzzleInput
+        puzzleInput.insert(0, 0)
+
+    def getDistribution(self):
+        differences = []
+        for i in range(1, len(self.jolts)):
+            differences.append(self.jolts[i]-self.jolts[i-1])
+
+        ones = differences.count(1)
+        twos = differences.count(2)
+        threes = 1 + differences.count(3)
+        return [ones, twos, threes]
+
+    def solve(self):
+        distribution = self.getDistribution()
+        return distribution[0]*distribution[2]
+
 
 def parseFile():
     import os
     relPath = 'PuzzleInput.txt'
-    absFilePath = os.path.abspath(__file__)
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
 
+
 def parsePuzzleInputFrom(file):
-    pass
+    result = []
+    for line in file.readlines():
+        result.append(int(line.strip('\n')))
+    result.sort()
+
+    return result
+
 
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
+    puzzleInput = parseFile()
     print(solver(puzzleInput).solve())
-
--- Problem10/tests.py
+++ Problem10/tests.py
@@ -1,8 +1,9 @@
 import unittest
-from solution  import solver
+from solution import solver
 from solution import parsePuzzleInputFrom
 
-def parseFile(filename = 'PuzzleInputTest.txt'):
+
+def parseFile(filename='PuzzleInputTest.txt'):
     import os
     relPath = filename
     absFilePath = os.path.dirname(__file__)
@@ -11,13 +12,48 @@ def parseFile(filename = 'PuzzleInputTest.txt'):
         result = parsePuzzleInputFrom(file)
     return result
 
+
 class getResults(unittest.TestCase):
-    pass
+    def test_simpleDistribution(self) -> None:
+        self.cut = solver([1, 2])
+        self.exp = [2, 0, 1]
+        self.assertDistribution()
+        self.assertEqual(self.cut.solve(), 2)
+
+    def assertDistribution(self):
+        act = self.cut.getDistribution()
+        self.assertEqual(act, self.exp)
+
+    def test_anotherSimpleDistribution(self) -> None:
+        self.cut = solver([1, 2, 3])
+        self.exp = [3, 0, 1]
+        self.assertDistribution()
+        self.assertEqual(self.cut.solve(), 3)
+
+    def test_aBitComplexDistribution(self) -> None:
+        self.cut = solver([1, 2, 3, 5])
+        self.exp = [3, 1, 1]
+        self.assertDistribution()
+
+    def test_aBitComplexDistribution(self) -> None:
+        self.cut = solver([1, 4, 5, 7])
+        self.exp = [2, 1, 2]
+        self.assertDistribution()
+
 
 class parsePuzzleInput(unittest.TestCase):
     def setUp(self) -> None:
         self.parsedInput = parseFile()
 
-    
+    def test_LowestElement(self):
+        self.assertEqual(self.parsedInput[0], 1)
+
+    def test_length(self):
+        self.assertEqual(len(self.parsedInput), 31)
+
+    def test_highestElement(self):
+        self.assertEqual(self.parsedInput[30], 49)
+
+
 if __name__ == '__main__':
     unittest.main()
