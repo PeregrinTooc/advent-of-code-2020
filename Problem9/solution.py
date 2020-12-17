083e534ad75f3e18d8f0df77d36f3cfe5b4cfa1d
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-17 15:18:44 +0100

    soilution 9

--- Problem9/solution.py
+++ Problem9/solution.py
@@ -1,28 +1,63 @@
+import os
+
+
+class NoKeyException(BaseException):
+    pass
+
+
 class solver:
-    def __init__(self,puzzleInput):
-        pass
+    def __init__(self, puzzleInput, preambleLength):
+        self.numbers = puzzleInput
+        self.preambleLength = preambleLength
 
     def solve(self):
-        pass
+        line = self.findVulnerableLine()
+        return self.numbers[line]
 
+    def findVulnerableLine(self):
+        line = 0
+        try:
+            for i in range(self.preambleLength + 1, len(self.numbers)):
+                line = i
+                self.findKeyForLine(line)
+        except NoKeyException:
+            pass
+        return line
+
+    def findKeyForLine(self, line):
+        keys = self.numbers[line-self.preambleLength:line]
+        for i in range(len(keys)):
+            for j in range(i+1, len(keys)):
+                candidate = [keys[i], keys[j]]
+                if candidate[0] + candidate[1] == self.numbers[line] and candidate[0] != candidate[1]:
+                    candidate.sort()
+                    return candidate
+
+        raise NoKeyException()
+
+    def findContinuousSequenceAddingUpTo(self, number):
+        pass
 
 
 def parseFile():
-    import os
     relPath = 'PuzzleInput.txt'
     absFilePath = os.path.abspath(__file__)
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
 
+
 def parsePuzzleInputFrom(file):
-    return None
-    
+    result = []
+    for line in file.readlines():
+        result.append(int(line.strip('\n')))
+    return result
+
 
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
-    print(solver(puzzleInput).solve())
-
+    puzzleInput = parseFile()
+    print(solver(puzzleInput, 25).findVulnerableLine())
+    print(solver(puzzleInput, 25).solve())
--- Problem9/tests.py
+++ Problem9/tests.py
@@ -1,8 +1,10 @@
 import unittest
-from solution  import solver
+from solution import solver
 from solution import parsePuzzleInputFrom
+from solution import NoKeyException
 
-def parseFile(filename = 'PuzzleInputTest.txt'):
+
+def parseFile(filename='PuzzleInputTest.txt'):
     import os
     relPath = filename
     absFilePath = os.path.dirname(__file__)
@@ -11,13 +13,57 @@ def parseFile(filename = 'PuzzleInputTest.txt'):
         result = parsePuzzleInputFrom(file)
     return result
 
+
 class getResults(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.parsedInput = parseFile()
+        self.cut = solver(self.parsedInput, 5)
+
+    def assertActualEquals(self, exp):
+        self.assertEqual(self.act, exp)
+
+    def test_findKeyForLineSix(self):
+        self.act = self.cut.findKeyForLine(5)
+        self.assertActualEquals([15, 25])
+
+    def test_findKeyForLineSeven(self):
+        self.act = self.cut.findKeyForLine(6)
+        self.assertActualEquals([15, 47])
+
+    def test_ExceptionOnNoKey(self):
+        with self.assertRaises(NoKeyException):
+            self.act = self.cut.findKeyForLine(14)
+
+    def test_Solve(self):
+        self.assertEqual(self.cut.solve(), 127)
+
+    def test_findVulnerableLine(self):
+        line = 0
+        try:
+            for i in range(6, 20):
+                line = i
+                self.cut.findKeyForLine(line)
+        except NoKeyException:
+            self.assertEqual(line, 14)
+
+    def test_findContinuousSequenceAddingUpTo(self):
+        self.cut.findContinuousSequenceAddingUpTo(127)
+        pass
+
 
 class parsePuzzleInput(unittest.TestCase):
     def setUp(self) -> None:
         self.parsedInput = parseFile()
 
-    
+    def test_firstLine(self):
+        self.assertEqual(self.parsedInput[0], 20)
+
+    def test_secondLine(self):
+        self.assertEqual(self.parsedInput[2], 15)
+
+    def test_fileLength(self):
+        self.assertEqual(len(self.parsedInput), 20)
+
+
 if __name__ == '__main__':
     unittest.main()
