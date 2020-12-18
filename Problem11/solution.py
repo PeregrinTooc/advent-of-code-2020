ba618ce64388e4bbec3937ec78f2ea41e0713761
Philipp Rembold <Philipp.Rembold@sap.com>
2020-12-18 15:04:39 +0100

    Solution 11 Part 1

--- Problem11/solution.py
+++ Problem11/solution.py
@@ -1,21 +1,136 @@
+import copy
+
+
 class solver:
-    pass
+    def __init__(self, grid=None, seats=None):
+        if grid:
+            self.grid = SeatingGrid(grid)
+        if seats:
+            self.grid = seats
+
+    def solve(self):
+        while not self.grid.isStable():
+            self.grid.tick()
+        return self.grid.getNumberOfOccupiedSeats()
+
+
+class SeatingGrid:
+    def __init__(self, grid):
+        self.seats = []
+        for row in grid:
+            parsedRow = []
+            for seat in row:
+                parsedRow.append(Seat(seat))
+            self.seats.append(parsedRow)
+        self.stable = False
+
+    def getNumberOfOccupiedSeats(self):
+        result = 0
+        for row in self.seats:
+            for seat in row:
+                if seat.isOccupied():
+                    result += 1
+        return result
+
+    def getSeatAt(self, column, row):
+        return self.seats[row][column]
+
+    def tick(self):
+        seats = copy.deepcopy(self.seats)
+        for i in range(len(seats)):
+            row = seats[i]
+            for j in range(len(row)):
+                seat = row[j]
+                numberOfNeighbors = self.getNumberOfOccupiedNeighbors(i, j)
+                if (numberOfNeighbors > 3 and seat.isOccupied()) or (numberOfNeighbors == 0 and not seat.isOccupied()):
+                    seat.switchOccupied()
+        self.checkStabilityWith(seats)
+        self.seats = seats
+
+    def checkStabilityWith(self, seats):
+        self.stable = True
+        for i in range(len(seats)):
+            row = seats[i]
+            for j in range(len(row)):
+                seat = row[j]
+                if not seat == self.seats[i][j]:
+                    self.stable = False
+                    return
+
+    def isStable(self):
+        return self.stable
+
+    def getNumberOfOccupiedNeighbors(self, row, column):
+        result = 0
+        if row > 0 and column > 0 and self.seats[row-1][column-1].isOccupied():
+            result += 1
+        if row > 0 and self.seats[row-1][column].isOccupied():
+            result += 1
+        if row > 0 and column < len(self.seats[row-1])-1 and self.seats[row-1][column+1].isOccupied():
+            result += 1
+        if column > 0 and self.seats[row][column-1].isOccupied():
+            result += 1
+        if column < len(self.seats[row])-1 and self.seats[row][column+1].isOccupied():
+            result += 1
+        if row < len(self.seats)-1 and column > 0 and self.seats[row+1][column-1].isOccupied():
+            result += 1
+        if row < len(self.seats)-1 and self.seats[row+1][column].isOccupied():
+            result += 1
+        if row < len(self.seats)-1 and column < len(self.seats[row+1])-1 and self.seats[row+1][column+1].isOccupied():
+            result += 1
+
+        return result
+
+
+class Seat:
+    def __init__(self, seat, occupied=False):
+        self.floor = seat == '.'
+        self.occupied = occupied
+
+    def __eq__(self, o: object) -> bool:
+        return o.floor == self.floor and self.occupied == o.occupied
+
+    def __copy__(self):
+        if self.isFloor():
+            seat = "."
+        else:
+            seat = 'L'
+        return Seat(seat, self.occupied)
+
+    def switchOccupied(self):
+        if self.isFloor():
+            return
+        if self.occupied:
+            self.occupied = False
+        else:
+            self.occupied = True
+
+    def isOccupied(self):
+        return self.occupied
+
+    def isFloor(self):
+        return self.floor
+
 
 def parseFile():
     import os
     relPath = 'PuzzleInput.txt'
-    absFilePath = os.path.abspath(__file__)
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
 
-    with open(absFilePath,'r') as file:
+    with open(absFilePath, 'r') as file:
         result = parsePuzzleInputFrom(file)
     return result
 
+
 def parsePuzzleInputFrom(file):
-    pass
+    grid = []
+    for line in file.readlines():
+        grid.append(line.strip('\n'))
+    result = SeatingGrid(grid)
+    return result
+
 
 if __name__ == '__main__':
-    puzzleInput = parseFile( )
-    print(solver(puzzleInput).solve())
-
+    puzzleInput = parseFile()
+    print(solver(seats=puzzleInput).solve())
--- Problem11/tests.py
+++ Problem11/tests.py
@@ -1,23 +1,145 @@
 import unittest
-from solution  import solver
-from solution import parsePuzzleInputFrom
 
-def parseFile(filename = 'PuzzleInputTest.txt'):
+import solution
+
+
+def parseFile(filename='PuzzleInputTest.txt'):
     import os
     relPath = filename
     absFilePath = os.path.dirname(__file__)
     absFilePath = os.path.join(absFilePath, relPath)
     with open(absFilePath, 'r') as file:
-        result = parsePuzzleInputFrom(file)
+        result = solution.parsePuzzleInputFrom(file)
     return result
+
 
 class getResults(unittest.TestCase):
     pass
+
+
+class identifyStable(unittest.TestCase):
+    def setUp(self):
+        self.gridSizeFourOnlySeats = [
+            ['L' for _ in range(4)] for _ in range(4)]
+
+    def test_oneTickSize4NoFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOnlySeats)
+        cut.tick()
+        self.assertFalse(cut.isStable())
+        cut.tick()
+        self.assertFalse(cut.isStable())
+        cut.tick()
+        self.assertTrue(cut.isStable())
+
+
+class countOccupiedWhenStable(unittest.TestCase):
+    def setUp(self):
+        self.gridSizeFourOnlySeats = [
+            ['L' for _ in range(4)] for _ in range(4)]
+        self.puzzleGrid = parseFile()
+
+    def test_SizeFourOnlySeats(self):
+        self.assertEqual(solution.solver(
+            grid=self.gridSizeFourOnlySeats).solve(), 4)
+
+    def test_SizeFourOnlySeats(self):
+        self.assertEqual(solution.solver(
+            seats=self.puzzleGrid).solve(), 37)
+
+
+class Tick(unittest.TestCase):
+    def setUp(self):
+        self.gridSizeFourOnlySeats = [
+            ['L' for _ in range(4)] for _ in range(4)]
+        self.gridSizeFourOneColumnFloor = [
+            ['L' for _ in range(4)] for _ in range(4)]
+        for i in range(4):
+            self.gridSizeFourOneColumnFloor[i][0] = "."
+
+    def test_oneTickSize4NoFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOnlySeats)
+        cut.tick()
+        for i in range(4):
+            for j in range(4):
+                self.assertTrue(cut.getSeatAt(i, j).isOccupied())
+
+    def test_twoTicksSize4NoFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOnlySeats)
+        for _ in range(2):
+            cut.tick()
+        self.assertTrue(cut.getSeatAt(0, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 3).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(0, 3).isOccupied())
+        for i in range(4):
+            for j in range(1, 3):
+                self.assertFalse(cut.getSeatAt(i, j).isOccupied())
+
+    def test_ThreeTicksSize4NoFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOnlySeats)
+        for _ in range(3):
+            cut.tick()
+        self.assertTrue(cut.getSeatAt(0, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 3).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(0, 3).isOccupied())
+        for i in range(4):
+            for j in range(1, 3):
+                self.assertFalse(cut.getSeatAt(i, j).isOccupied())
+
+    def test_oneTickSize41ColumnFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOnlySeats)
+        cut.tick()
+        for i in range(4):
+            for j in range(1, 4):
+                self.assertTrue(cut.getSeatAt(i, j).isOccupied())
+            self.assertTrue(cut.getSeatAt(i, 0).isOccupied())
+
+    def test_twoTicksSize41ColumnFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOneColumnFloor)
+        cut.tick()
+        for i in range(4):
+            for j in range(1, 4):
+                self.assertTrue(cut.getSeatAt(i, j).isOccupied())
+            self.assertFalse(cut.getSeatAt(i, 0).isOccupied())
+
+    def test_twoTicksSize41ColumnFloor(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOneColumnFloor)
+        for _ in range(2):
+            cut.tick()
+        self.assertTrue(cut.getSeatAt(1, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 3).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(1, 3).isOccupied())
+
+    def test_threeTicksSize41ColumnFloorStable(self):
+        cut = solution.SeatingGrid(self.gridSizeFourOneColumnFloor)
+        for _ in range(3):
+            cut.tick()
+        self.assertTrue(cut.getSeatAt(1, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 3).isOccupied())
+        self.assertTrue(cut.getSeatAt(3, 0).isOccupied())
+        self.assertTrue(cut.getSeatAt(1, 3).isOccupied())
+
 
 class parsePuzzleInput(unittest.TestCase):
     def setUp(self) -> None:
         self.parsedInput = parseFile()
 
-    
+    def test_firstSeat(self):
+        self.assertEqual(self.parsedInput.getSeatAt(0, 0).isOccupied(), False)
+
+    def test_firstSeatNotFloor(self):
+        self.assertEqual(self.parsedInput.getSeatAt(0, 0).isFloor(), False)
+
+    def test_secondSeat(self):
+        self.assertEqual(self.parsedInput.getSeatAt(1, 0).isOccupied(), False)
+        self.assertEqual(self.parsedInput.getSeatAt(1, 0).isFloor(), True)
+
+    def test_lastSeat(self):
+        self.assertEqual(self.parsedInput.getSeatAt(9, 9).isOccupied(), False)
+        self.assertEqual(self.parsedInput.getSeatAt(9, 9).isFloor(), False)
+
+
 if __name__ == '__main__':
     unittest.main()
