28c6bea03187a0ef9241f9d49bc36bfbe081c425
Philipp Rembold <Philipp.Rembold@sap.com>
2022-12-09 15:33:30 +0100

    Add Template and begin day 7

--- 2022/src/day8/Solver.java
+++ 2022/src/day8/Solver.java
@@ -2,4 +2,23 @@ package day8;
 
 public class Solver {
 
+    private int[][] input;
+
+    public Solver(String[] input) {
+        this.input = new int[input.length][input.length];
+        for (int i = 0; i < this.input.length; i++) {
+            for (int j = 0; j < this.input[i].length; j++) {
+                this.input[i][j] = Integer.valueOf(input[i].substring(j, j + 1));
+            }
+        }
+    }
+
+    public Integer solve1() {
+        if (input.length <= 1) {
+            return 1;
+        }
+        int result = 2 * input.length + 2 * (input.length - 2);
+        result += input[1][1];
+        return result;
+    }
 }
--- 2022/src/day8/allTests.java
+++ 2022/src/day8/allTests.java
@@ -1,13 +1,14 @@
 package day8;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.net.URL;
 
-// import util.Utils;
+import util.Utils;
 
 public class allTests {
     static File testFile = null;
@@ -23,20 +24,23 @@ public class allTests {
     }
 
     @Test
+    @Disabled
     void acceptance() {
-        /*
-         * String[] input = Utils.transform(testFile);
-         * assertEquals(2, new Solver(input).solve1());
-         * assertEquals(4, new Solver(input).solve2());
-         * input = Utils.transform(realFile);
-         * assertEquals(498, new Solver(input).solve1());
-         * assertEquals(859, new Solver(input).solve2());
-         */
+        String[] input = Utils.transform(testFile);
+        assertEquals(21, new Solver(input).solve1());
+        // assertEquals(4, new Solver(input).solve2());
+        // input = Utils.transform(realFile);
+        // assertEquals(498, new Solver(input).solve1());
+        // assertEquals(859, new Solver(input).solve2());
+
     }
 
     @Test
     void firstTest() {
-        assertEquals(1 + 1, 2);
+        assertEquals(1, new Solver(new String[] { "0" }).solve1());
+        assertEquals(4, new Solver(new String[] { "09", "90" }).solve1());
+        assertEquals(8, new Solver(new String[] { "000", "000", "000" }).solve1());
+        assertEquals(9, new Solver(new String[] { "000", "010", "000" }).solve1());
     }
 
 }
