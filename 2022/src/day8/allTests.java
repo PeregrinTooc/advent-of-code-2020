aad65eadc2f94d2456b61528c87e9e7d2ab1f3e7
Philipp Rembold <Philipp.Rembold@sap.com>
2022-12-09 12:58:39 +0100

    structure for days 8 and 9

---
+++ 2022/src/day8/allTests.java
@@ -0,0 +1,42 @@
+package template;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.net.URL;
+
+// import util.Utils;
+
+public class allTests {
+    static File testFile = null;
+    static File realFile = null;
+    Solver solver = null;
+
+    @BeforeAll
+    static void classSetUp() {
+        URL path = allTests.class.getResource("test.txt");
+        testFile = new File(path.getFile());
+        path = allTests.class.getResource("input.txt");
+        realFile = new File(path.getFile());
+    }
+
+    @Test
+    void acceptance() {
+        /*
+         * String[] input = Utils.transform(testFile);
+         * assertEquals(2, new Solver(input).solve1());
+         * assertEquals(4, new Solver(input).solve2());
+         * input = Utils.transform(realFile);
+         * assertEquals(498, new Solver(input).solve1());
+         * assertEquals(859, new Solver(input).solve2());
+         */
+    }
+
+    @Test
+    void firstTest() {
+        assertEquals(1 + 1, 2);
+    }
+
+}
