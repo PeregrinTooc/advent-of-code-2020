41a57b9df286d9aab6651abdd35e7d6ca487f200
philipp <philipp.rembold@gmail.com>
2022-12-10 11:32:51 +0100

    day 8 part1

--- 2022/src/day8/Solver.java
+++ 2022/src/day8/Solver.java
@@ -1,24 +1,76 @@
 package day8;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Solver {
 
-    private int[][] input;
+    private final int[][] mapOfTrees;
 
-    public Solver(String[] input) {
-        this.input = new int[input.length][input.length];
-        for (int i = 0; i < this.input.length; i++) {
-            for (int j = 0; j < this.input[i].length; j++) {
-                this.input[i][j] = Integer.valueOf(input[i].substring(j, j + 1));
+    public Solver(final String @NotNull [] input) {
+        mapOfTrees = new int[input.length][input.length];
+        for (int i = 0; i < mapOfTrees.length; i++) {
+            for (int j = 0; j < mapOfTrees[i].length; j++) {
+                mapOfTrees[i][j] = Integer.valueOf(input[i].substring(j, j + 1));
             }
         }
     }
 
     public Integer solve1() {
-        if (input.length <= 1) {
+        if (mapOfTrees.length <= 1) {
             return 1;
         }
-        int result = 2 * input.length + 2 * (input.length - 2);
-        result += input[1][1];
+        int result = 2 * this.mapOfTrees.length + 2 * (this.mapOfTrees.length - 2);
+        for (int i = 1; i < this.mapOfTrees.length - 1; i++) {
+            for (int j = 1; j < this.mapOfTrees.length - 1; j++) {
+                if (isBiggerThanSurroundings(i, j))
+                    result += 1;
+            }
+        }
         return result;
+    }
+
+    private boolean isBiggerThanSurroundings(int i, int j) {
+        return isBiggerThanAllLeft(i, j) || isBiggerThanAllRight(i, j)
+                || isBiggerThanAllAbove(i, j) || isBiggerThanAllBelow(i, j);
+    }
+
+    private boolean isBiggerThanAllBelow(int i, int j) {
+        for (int k = j + 1; k < mapOfTrees.length; k++) {
+            if (this.mapOfTrees[i][j] <= this.mapOfTrees[i][k]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBiggerThanAllAbove(int i, int j) {
+        for (int k = j - 1; k >= 0; k--) {
+            if (this.mapOfTrees[i][j] <= this.mapOfTrees[i][k]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBiggerThanAllRight(int i, int j) {
+        for (int k = i + 1; k < mapOfTrees.length; k++) {
+            if (this.mapOfTrees[i][j] <= this.mapOfTrees[k][j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBiggerThanAllLeft(int i, int j) {
+        for (int k = i - 1; k >= 0; k--) {
+            if (this.mapOfTrees[i][j] <= this.mapOfTrees[k][j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int solve2() {
+        return 0;
     }
 }
--- 2022/src/day8/allTests.java
+++ 2022/src/day8/allTests.java
@@ -3,12 +3,12 @@ package day8;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import util.Utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.net.URL;
 
-import util.Utils;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class allTests {
     static File testFile = null;
@@ -23,24 +23,51 @@ public class allTests {
         realFile = new File(path.getFile());
     }
 
+    private static void assertSolution1(String[] input, int expected) {
+        assertEquals(expected, new Solver(input).solve1());
+    }
+
+    private static void assertSolution2(String[] input, int expected) {
+        assertEquals(expected, new Solver(input).solve2());
+    }
+
     @Test
     @Disabled
     void acceptance() {
         String[] input = Utils.transform(testFile);
-        assertEquals(21, new Solver(input).solve1());
-        // assertEquals(4, new Solver(input).solve2());
-        // input = Utils.transform(realFile);
-        // assertEquals(498, new Solver(input).solve1());
+        assertSolution1(input, 21);
+        assertSolution2(input, 8);
+        input = Utils.transform(realFile);
+        assertSolution1(input, 1560);
         // assertEquals(859, new Solver(input).solve2());
 
     }
 
     @Test
-    void firstTest() {
-        assertEquals(1, new Solver(new String[] { "0" }).solve1());
-        assertEquals(4, new Solver(new String[] { "09", "90" }).solve1());
-        assertEquals(8, new Solver(new String[] { "000", "000", "000" }).solve1());
-        assertEquals(9, new Solver(new String[] { "000", "010", "000" }).solve1());
+    void directSurroundings() {
+        assertSolution1(new String[]{"0"}, 1);
+        assertSolution1(new String[]{"09", "90"}, 4);
+        assertSolution1(new String[]{"000", "000", "000"}, 8);
+        assertSolution1(new String[]{"000", "010", "000"}, 9);
+        assertSolution1(new String[]{"010", "111", "010"}, 8);
+        assertSolution1(new String[]{"000", "111", "010"}, 9);
+        assertSolution1(new String[]{"010", "011", "010"}, 9);
+        assertSolution1(new String[]{"010", "110", "010"}, 9);
+        assertSolution1(new String[]{"010", "111", "000"}, 9);
     }
+
+    @Test
+    void squareOfFour() {
+        assertSolution1(new String[]{"0000", "0110", "0110", "0000"}, 16);
+        assertSolution1(new String[]{"1111", "1110", "0111", "1111"}, 14);
+    }
+
+    @Test
+    void squareOfFive() {
+        assertSolution1(new String[]{"00000", "01210", "02320", "01210", "00000"}, 25);
+        assertSolution1(new String[]{"00300", "01310", "32323", "01310", "00300"}, 24);
+        assertSolution1(new String[]{"99999", "91319", "92329", "91319", "99999"}, 16);
+    }
+
 
 }
