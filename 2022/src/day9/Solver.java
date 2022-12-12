a6589909de348cf83f55d27c02af8f105908a302
Philipp Rembold <Philipp.Rembold@sap.com>
2022-12-12 11:51:13 +0100

    day 9 wip

--- 2022/src/day9/Solver.java
+++ 2022/src/day9/Solver.java
@@ -1,5 +1,42 @@
 package day9;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.javatuples.Pair;
+
 public class Solver {
+
+    private List<Pair<Direction, Integer>> instructions;
+
+    public Solver(String[] input) {
+        instructions = new ArrayList<Pair<Direction, Integer>>();
+        for (int i = 0; i < input.length; i++) {
+            String[] s = input[i].split(" ");
+            instructions.add(new Pair<Direction, Integer>(null, Integer.valueOf(s[1])));
+        }
+    }
+
+    public int solve1() {
+        return 0;
+    }
+
+    public class mapOfHeadAndTail {
+
+        public int countTouchedSpots() {
+            int result = 1;
+            if (instructions.size() > 0) {
+                result = instructions.get(0).getValue1();
+            }
+            if (instructions.size() > 1) {
+                result += instructions.get(1).getValue1() - 1;
+            }
+            return result;
+        }
+
+    }
+
+    private class Direction {
+    }
 
 }
--- 2022/src/day9/allTests.java
+++ 2022/src/day9/allTests.java
@@ -1,13 +1,14 @@
 package day9;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.net.URL;
 
-// import util.Utils;
+import util.Utils;
 
 public class allTests {
     static File testFile = null;
@@ -23,20 +24,30 @@ public class allTests {
     }
 
     @Test
+    @Disabled
     void acceptance() {
-        /*
-         * String[] input = Utils.transform(testFile);
-         * assertEquals(2, new Solver(input).solve1());
-         * assertEquals(4, new Solver(input).solve2());
-         * input = Utils.transform(realFile);
-         * assertEquals(498, new Solver(input).solve1());
-         * assertEquals(859, new Solver(input).solve2());
-         */
+
+        String[] input = Utils.transform(testFile);
+        assertEquals(13, new Solver(input).solve1());
+        // assertEquals(4, new Solver(input).solve2());
+        input = Utils.transform(realFile);
+        // assertEquals(498, new Solver(input).solve1());
+        // assertEquals(859, new Solver(input).solve2());
+
     }
 
     @Test
-    void firstTest() {
-        assertEquals(1 + 1, 2);
+    void lessThanThreeInstructions() {
+        assertCount(new String[] {}, 1);
+        assertCount(new String[] { "R 1" }, 1);
+        assertCount(new String[] { "R 2" }, 2);
+        assertCount(new String[] { "R 2", "U 1" }, 2);
+        assertCount(new String[] { "R 2", "U 2" }, 3);
+    }
+
+    private void assertCount(String[] input, int expectedResult) {
+        Solver.mapOfHeadAndTail map = new Solver(input).new mapOfHeadAndTail();
+        assertEquals(expectedResult, map.countTouchedSpots());
     }
 
 }
