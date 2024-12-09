4c691b3d01b9d43e4988b9dfd171a9c5d5941705
Philipp Rembold <philipp.rembold@googlemail.com>
2024-12-09 17:46:55 +0100

    setup for day 5

---
+++ 2024/advent-of-code/src/advent_of_code/day5/solver.clj
@@ -0,0 +1,12 @@
+(ns advent-of-code.day5.solver
+  (:require [clojure.string :as str]))
+
+(defn solve1 [input]
+  )
+
+(defn solve2 [input]
+  )
+
+
+
+
---
+++ 2024/advent-of-code/test/advent_of_code/day5/solver_test.clj
@@ -0,0 +1,15 @@
+(ns advent-of-code.day5.solver-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.day1.solver :refer :all]
+            [advent-of-code.util.util :refer :all]))
+
+(def input (transform-file-to-list-of-lines "resources/advent_of_code/day5/input.txt"))
+
+
+(deftest all-tests-part1
+  (testing "Acceptance Test"
+    (is true)
+    )
+  )
+
+
