c123f4c95134a64127383abef7aedb7c66b46183
Philipp Rembold <philipp.rembold@googlemail.com>
2024-12-09 22:23:25 +0100

    day 5 part 2 wip

--- 2024/advent-of-code/src/advent_of_code/day5/solver.clj
+++ 2024/advent-of-code/src/advent_of_code/day5/solver.clj
@@ -1,11 +1,53 @@
 (ns advent-of-code.day5.solver
   (:require [clojure.string :as str]))
 
+
+(defn create-rules-and-pages [input]
+  (if (< (count input) 2)
+    []
+    (let [[rules limit-and-pages] (split-with #(not= % "") input)
+          pages (rest limit-and-pages)]
+      [rules pages]))
+  )
+
+(defn middle-of [v]
+  (v (/ (dec (count v)) 2))
+  )
+
+(defn matches-rule [pages-string rule-string]
+  (let [pages (map read-string (str/split pages-string #","))
+        [comes-first comes-after] (mapv read-string (str/split rule-string #"\|"))
+        matched-pages (filterv #(or (= comes-first %) (= comes-after %)) pages)
+        ]
+    (or (< (count matched-pages) 2) (= [comes-first comes-after] matched-pages))
+    )
+  )
+
+(defn matches-all-rules [rules pages]
+  (every? (partial matches-rule pages) rules)
+  )
+
+(defn sort-with [rule-strings page1 page2]
+  (let [rules (mapv #(mapv read-string (str/split % #"\|")) rule-strings)
+        [comes-first comes-after] (rules 0)]
+    (cond (= page1 comes-first) -1 :else 1)
+    )
+  )
+
 (defn solve1 [input]
+  (let [[rules pages] (create-rules-and-pages input)
+        rule-abiding-pages (filter (partial matches-all-rules rules) pages)]
+    (reduce + (map (comp middle-of #(mapv read-string (str/split % #","))) rule-abiding-pages))
+    )
   )
 
 (defn solve2 [input]
+  (let [[rules pages] (create-rules-and-pages input)
+        naughty-pages (filter (complement (partial matches-all-rules rules)) pages)]
+    (apply + (map #(middle-of (sort (partial sort-with rules) (mapv read-string (str/split % #",")))) naughty-pages))
+    )
   )
+
 
 
 
--- 2024/advent-of-code/test/advent_of_code/day5/solver_test.clj
+++ 2024/advent-of-code/test/advent_of_code/day5/solver_test.clj
@@ -1,15 +1,69 @@
 (ns advent-of-code.day5.solver-test
   (:require [clojure.test :refer :all]
-            [advent-of-code.day1.solver :refer :all]
+            [advent-of-code.day5.solver :refer :all]
             [advent-of-code.util.util :refer :all]))
 
 (def input (transform-file-to-list-of-lines "resources/advent_of_code/day5/input.txt"))
-
+(def acceptance-input ["47|53"
+                       "97|13"
+                       "97|61"
+                       "97|47"
+                       "75|29"
+                       "61|13"
+                       "75|53"
+                       "29|13"
+                       "97|29"
+                       "53|29"
+                       "61|53"
+                       "97|53"
+                       "61|29"
+                       "47|13"
+                       "75|47"
+                       "97|75"
+                       "47|61"
+                       "75|61"
+                       "47|29"
+                       "75|13"
+                       "53|13"
+                       ""
+                       "75,47,61,53,29"
+                       "97,61,53,29,13"
+                       "75,29,13"
+                       "75,97,47,61,53"
+                       "61,13,29"
+                       "97,13,75,29,47"])
 
 (deftest all-tests-part1
   (testing "Acceptance Test"
-    (is true)
-    )
+    (is (= 143 (solve1 acceptance-input)))
+    (is (= 3608 (solve1 input))))
   )
+
+(deftest unit-tests
+  (testing "input transformation"
+    (is (= [] (create-rules-and-pages [])))
+    (is (= [] (create-rules-and-pages [""])))
+    (is (= [["53|13"] []] (create-rules-and-pages ["53|13" ""])))
+    (is (= [["53|13"] ["21"]] (create-rules-and-pages ["53|13" "" "21"])))
+    (is (= [["75|13" "53|13"] ["75,47,61,53,29" "97,61,53,29,13"]]
+           (create-rules-and-pages ["75|13" "53|13" "" "75,47,61,53,29" "97,61,53,29,13"])))
+    )
+  (testing "rule-matching"
+    (is (= true (matches-rule "3,4" "1|2")) "1|2 is not applicable to 3,4")
+    (is (= true (matches-rule "1,4" "1|2")) "1|2 is not applicable to 1,4")
+    (is (= false (matches-rule "2,1" "1|2")) "2,1 does not fulfill 1|2")
+    (is (= true (matches-rule "1,2" "1|2")) "1,2 fulfills 1|2")
+    )
+  (testing "receiving middle element"
+    (is (= 1 (middle-of [1])))
+    (is (= 1 (middle-of [2 1 "3"])))
+    (is (= "75" (middle-of ["97" "13" "75" "29" "47"])))
+    )
+  (testing "sort by rule")
+  (is (= -1 (sort-with ["1|2"] 1 2)) "1,2 does already fulfill 1|2")
+  (is (= 1 (sort-with ["1|2"] 2 1)) "1 must come before 2 according to 1|2")
+  (is (= 1 (sort-with ["1|2" "2|4"] 4 2)) "4 must come before 2 according to 4|2")
+  )
+
 
 
