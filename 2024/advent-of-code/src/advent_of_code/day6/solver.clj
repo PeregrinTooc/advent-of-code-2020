309725fb6e72501434f3f6d38fa3cb716a6366ae
Philipp Rembold <philipp.rembold@googlemail.com>
2024-12-16 22:45:11 +0100

    day6 wip

--- 2024/advent-of-code/src/advent_of_code/day6/solver.clj
+++ 2024/advent-of-code/src/advent_of_code/day6/solver.clj
@@ -1,13 +1,80 @@
 (ns advent-of-code.day6.solver
   (:require [clojure.string :as str]))
 
-(defn step [current-situation]
-  (mapv #(str/replace #"\^" "X" %) current-situation)
+
+
+(defn- line-to-cells [line-index line]
+  (map-indexed (fn [column-index cell-value]
+                 [[line-index column-index] cell-value])
+               line))
+
+(defn- add-line-to-matrix [matrix [line-index line]]
+  (into matrix (line-to-cells line-index line)))
+
+(defn transform-to-cells [lines]
+  (let [indexed-lines (map-indexed vector lines)]
+    (reduce add-line-to-matrix {} indexed-lines)))
+(defn- turn [direction]
+  (let [next-directions {\^ \>, \> \v, \v \<, \< \^}]
+    (next-directions direction)
+    ))
+(defn- calculate-next-position [line column direction]
+  (case direction
+    \^ [(dec line) column]
+    \v [(inc line) column]
+    \> [line (inc column)]
+    \< [line (dec column)]
+    )
+  )
+
+(defn- create-next-map [marked-position [line column] direction]
+  (let [next-position (marked-position (calculate-next-position line column direction))]
+    (if (= \# next-position)
+      (assoc marked-position [line column] (turn direction))
+      (assoc marked-position [(dec line) column] direction)
+      )
+    )
+  )
+(defn- end-of-patrol? [marked-position line column direction]
+  (let [next-position (calculate-next-position line column direction)]
+    (nil? (marked-position next-position)))
+  )
+
+(defn- move-guard [current-situation [line column]]
+  (let [direction (current-situation [line column])
+        marked-position (assoc current-situation [line column] \X)
+        ]
+    (if (end-of-patrol? marked-position line column direction)
+      marked-position
+      (create-next-map marked-position [line column] direction)
+      )
+    )
   )
 
 
-(defn solve1 [input]
+(defn step [current-situation]
+  (let [position-of-guard (some (fn [[k v]] (when (contains? #{\^ \> \< \v} v) k)) current-situation)]
+    (if (nil? position-of-guard)
+      current-situation
+      (move-guard current-situation position-of-guard)
+      )
+    )
+  )
 
+(defn- repeat-until-stable [f lst]
+  (loop [lst lst]
+    (let [first-item (first lst)
+          second-item (second lst)]
+      (if (= first-item second-item)
+        lst
+        (recur (cons (f first-item) lst))))))
+
+
+(defn solve1 [input]
+  (let [lines-as-cells (transform-to-cells input)
+        all-steps (repeat-until-stable step (list lines-as-cells))]
+    (filter (fn [k v]) all-steps)
+    )
   )
 
 (defn solve2 [input]
--- 2024/advent-of-code/test/advent_of_code/day6/solver_test.clj
+++ 2024/advent-of-code/test/advent_of_code/day6/solver_test.clj
@@ -30,18 +30,80 @@
     )
   )
 
+(defn group-by-row [cells-map]
+  "Groups the map entries by row index."
+  (group-by (fn [[[row _] _]] row) cells-map))
 
+(defn sort-row [row]
+  "Sorts a single row by column index and extracts cell values."
+  (->> row
+       (sort-by (fn [[[row col] _]] col))                   ;; Sort by column index
+       (map (fn [[[_ _] value]] value))))                   ;; Extract cell values
+
+(defn combine-rows [grouped]
+  "Combines sorted rows into strings to reconstruct the original array."
+  (->> (sort-by key grouped)                                ;; Sort by row index
+       (map (fn [[_ row]]                                   ;; Process each group
+              (->> (sort-row row)                           ;; Sort columns in the row
+                   (apply str))))))                         ;; Combine values into a single string
+
+(defn cells-to-array [cells-map]
+  "Transforms a map of cells back into an array of strings."
+  (-> cells-map
+      group-by-row                                          ;; Group by row index
+      combine-rows                                          ;; Combine rows into strings
+      vec))                                                 ;; Return as a vector
+
+(def idx (comp cells-to-array transform-to-cells))
+(def read-after-step (comp cells-to-array step transform-to-cells))
 
 (deftest unit-tests
   (testing "one step at a time"
     (is (= [".X"
-            ".."] (step [".^"
-                         ".."])))
+            ".."] (read-after-step [".^"
+                                    ".."])))
+    (is (= [".X"
+            ".."] (read-after-step [".X"
+                                    ".."])) "should not do anything if no guard is present")
     (is (= [".^"
-            ".X"] (step [".X"
-                         ".^"])))
+            ".X"] (read-after-step [".X"
+                                    ".^"])) "leaves at the top if at the end of map")
+    (is (= ["#."
+            ">."] (read-after-step ["#."
+                                    "^."])) "turns right by 90° if blocked")
+    (is (= ["#."
+            "v#"] (read-after-step ["#."
+                                    ">#"])) "turns downwards if blocked to the right")
+    (is (= ["#<"
+            ".#"] (read-after-step ["#v"
+                                    ".#"])) "turns to left if blocked to the bottom")
+    (is (= ["#^"
+            ".#"] (read-after-step ["#<"
+                                    ".#"])) "turns upwards if blocked to the left")
+    (is (= ["#."
+            ".X"] (read-after-step ["#."
+                                    ".>"])) "leaves to the right if at end of map")
+    (is (= ["#."
+            "X."] (read-after-step ["#."
+                                    "<."])) "leaves to the left if at end of map")
+    (is (= ["#."
+            "X."] (read-after-step ["#."
+                                    "v."])) "leaves to the bottom if at end of map")
     )
+  (testing "transform to cells"
+    (is (= {[0 0] \X} (transform-to-cells ["X"])))
+    (is (= {[0 0] \X, [0 1] \.} (transform-to-cells ["X."])))
+    (is (= {[0 0] \A, [0 1] \B
+            [1 0] \C, [1 1] \D} (transform-to-cells ["AB" "CD"])))
+    (is (= {[0 0] \A, [0 1] \B [0 2] \C
+            [1 0] \D, [1 1] \E [1 2] \F
+            [2 0] \G, [2 1] \H [2 2] \I} (transform-to-cells ["ABC" "DEF" "GHI"])))
+    )
+  (testing "re-transfomration"
 
+    (is (= ["aBc"] (idx ["aBc"]))
+        )
+    )
   )
 
 
