f19418a204e15c50088d24c96cf22a22af773abe
Philipp Rembold <philipp.rembold@googlemail.com>
2024-12-23 23:26:10 +0100

    day 8 part one done

--- 2024/advent-of-code/src/advent_of_code/day8/solver.clj
+++ 2024/advent-of-code/src/advent_of_code/day8/solver.clj
@@ -1,9 +1,42 @@
-(ns advent-of-code.template.solver
-  (:require [clojure.string :as str]))
+(ns advent-of-code.day8.solver
+  (:require [clojure.math.combinatorics :as combo]
+            [advent-of-code.util.util :refer :all]))
 
+(defn extract-all-antenna-positions [input]
+  (->>
+    input
+    (transform-to-cells)
+    (filter #(not (= (second %) \.
+                     )))
+    (into {})
+    )
+  )
+(defn- in-bound? [size [x y]]
+  (and (< x size) (< y size) (<= 0 x) (<= 0 y))
+  )
+
+(defn one-antinode [x1 x2 y1 y2]
+  [(- (+ x1 x1) x2) (- (+ y1 y1) y2)])
+
+(defn- determine-antinodes [[[x1 y1] [x2 y2]] size]
+  (set (filter (partial in-bound? size) [(one-antinode x1 x2 y1 y2) (one-antinode x2 x1 y2 y1)]))
+  )
+
+(defn antinodes [frequency antenna-positions map-size]
+  (let [generating-antennas (map key (filter #(= (second %) frequency) antenna-positions))]
+    (->> (combo/combinations generating-antennas 2)         ;; Generate all combinations
+         (map #(determine-antinodes (vec %) map-size))      ;; Compute antinodes for each combination
+         (reduce into #{}))))                               ;; Merge all results into a single set
 
 (defn solve1 [input]
+  (let [antenna-positions (extract-all-antenna-positions input)
+        frequencies (set (map val antenna-positions))]
+    (println antenna-positions)
+    (println frequencies)
+    (count (set (reduce concat (map #(antinodes % antenna-positions (count input)) frequencies))))
+    )
   )
+
 
 (defn solve2 [input]
   )
--- 2024/advent-of-code/test/advent_of_code/day8/solver_test.clj
+++ 2024/advent-of-code/test/advent_of_code/day8/solver_test.clj
@@ -1,12 +1,38 @@
 (ns advent-of-code.day8.solver-test
   (:require [clojure.test :refer :all]
-            [advent-of-code.template.solver :refer :all]
+            [advent-of-code.day8.solver :refer :all]
             [advent-of-code.util.util :refer :all]))
 
-(def input (transform-file-to-list-of-lines "resources/advent_of_code/template/input.txt"))
+(def input (transform-file-to-list-of-lines "resources/advent_of_code/day8/input.txt"))
+(def acceptance-input (transform-file-to-list-of-lines "resources/advent_of_code/day8/acceptance.txt"))
 
+(deftest acceptance-testing
+  (testing "part 1"
+    (is (= 14 (solve1 acceptance-input)))
+    (is (= 265 (solve1 input)))
+    )
+  (testing "part 2"
+    (is (= 34 (solve2 acceptance-input)))
+    (is (= nil (solve2 input)))
+    )
+  )
+(deftest unit-testing
+  (testing "all antennas with their locations"
+    (is (= (into {} [[[8 8] \A]
+                     [[2 5] \0]
+                     [[9 9] \A]
+                     [[1 8] \0]
+                     [[5 6] \A]
+                     [[4 4] \0]
+                     [[3 7] \0]]) (extract-all-antenna-positions acceptance-input)))
+    )
+  (testing "antinodes"
+    (is (= (set [[7 7] [10 10]]) (antinodes \A (into {} [[[8 8] \A] [[9 9] \A] [[4 5] \0]]) 12)) "only looks for the given frequency")
+    (is (= (set [[7 7] [10 10]]) (antinodes \A (into {} [[[8 8] \A] [[9 9] \A]]) 12)))
+    (is (= (set [[2 2] [5 5] [1 4] [3 6] [6 0] [7 1]]) (antinodes \A (into {} [[[5 2] \A] [[3 3] \A] [[4 4] \A]]) 12)))
+    (is (= (set [[7 7]]) (antinodes \A (into {} [[[8 8] \A] [[9 9] \A]]) 9)) "discard those out of bounds of the map")
+    (is (= (set [[2 2]]) (antinodes \A (into {} [[[0 0] \A] [[1 1] \A]]) 9)) "discard those out of bounds of the map")
 
-(deftest "acceptance testing")
-(deftest "unit testing")
+    )
 
-
+  )
