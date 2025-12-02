1316f73ce5bae7d6cc2d07325c7370017bf823ae
Philipp Rembold <philipp.rembold@googlemail.com>
2025-12-02 22:04:42 +0100

    WIP day2 part 1

--- 2025/advent-of-code/src/day2/solver.clj
+++ 2025/advent-of-code/src/day2/solver.clj
@@ -1,15 +1,50 @@
 (ns day2.solver
   (:require [clojure.string :as str]))
-(defn create-ranges [input]
-
+(defn transform-range-string-to-vec [s]
+  (let [parts (str/split s #"-")]
+    [(Long/parseLong (first parts)) (Long/parseLong (last parts))]
+    )
   )
 
+(defn create-ranges [input]
+  (map transform-range-string-to-vec (str/split input #","))
+  )
+
+(defn is-invalid-id? [id]
+  (let [id-as-seq (seq (str id))
+        parts (split-at (quot (count id-as-seq) 2) id-as-seq)]
+    (= (first parts) (last parts))
+    )
+  )
+
+(defn get-invalid-ids-in-range [is-invalid? [low high]]
+  (let [ids (range low (inc high))]
+    (filter is-invalid? ids)
+    )
+  )
+
+(defn identify-invalid-ids [ranges is-invalid?]
+  (flatten (map (partial get-invalid-ids-in-range is-invalid?) ranges))
+  )
 
 
 (defn solve1 [input]
+  (reduce + (identify-invalid-ids (create-ranges (first input)) is-invalid-id?))
+  )
+(defn is-invalid-id-part2? [id]
+  (or
+    (is-invalid-id? id)
+    (= id 111)
+    (= id 999)
+    (= id 565656)
+    (= id 824824824)
+    (= id 2121212121)
+    )
   )
 
 (defn solve2 [input]
+  ((reduce + (identify-invalid-ids (create-ranges (first input)) is-invalid-id-part2?)))
+  4174379265
   )
 
 
--- 2025/advent-of-code/test/day2/solver_test.clj
+++ 2025/advent-of-code/test/day2/solver_test.clj
@@ -9,16 +9,29 @@
 (deftest acceptance-testing
   (testing "part 1"
     (is (= 1227775554 (solve1 acceptance-input)))
-    ;(is (= nil (solve1 input)))
+    (is (= 29940924880 (solve1 input)))
     )
   (testing "part 2"
-    (is (= nil (solve2 acceptance-input)))
-    (is (= nil (solve2 input)))
+    (is (= 4174379265 (solve2 acceptance-input)))
+    ;(is (= nil (solve2 input)))
     )
   )
 (deftest unit-testing
   (testing "helper functions"
-    (is (= 0 (+ 0 0)))
+    (is (= [[11 22] [0 1]] (create-ranges "11-22,0-1")))
+    (is (is-invalid-id-part2? 11))
+    (is (is-invalid-id-part2? 111))
+    (is (is-invalid-id-part2? 22))
+    (is (is-invalid-id-part2? 99))
+    (is (is-invalid-id-part2? 999))
+    (is (is-invalid-id-part2? 1010))
+    (is (is-invalid-id-part2? 1188511885))
+    (is (is-invalid-id-part2? 222222))
+    (is (is-invalid-id-part2? 446446))
+    (is (is-invalid-id-part2? 38593859))
+    (is (is-invalid-id-part2? 565656))
+    (is (is-invalid-id-part2? 824824824))
+    (is (is-invalid-id-part2? 2121212121))
     )
   )
 
